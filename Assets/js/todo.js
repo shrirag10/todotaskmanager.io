8602f6ef0c87bce4a2e4f7d2ab54589b6c816192
shrirag10 <shrirag10@gmail.com>
2020-06-04 08:30:52 +0530

    update
    fkakf

---
+++ Assets/js/todo.js
@@ -0,0 +1,28 @@
+// 
+$("ul").on("click","li",function()
+{
+	$(this).toggleClass("completed");
+}); 
+
+$("ul").on("click","span",function(e)
+{	
+	$(this).parent().fadeOut(500,function()
+	{
+		$(this).remove();
+	});
+	e.stopPropagation();
+});
+
+$("input").keypress(function(e)
+{
+	if (e.which===13) 
+	{ 	var todoText = $(this).val();
+		 $(this).val("");
+		 $("ul").append("<li><span><i class='fa fa-trash'></i></span> "+ todoText +"</li>");
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input").fadeToggle();
+
+});
